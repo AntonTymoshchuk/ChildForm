d537c77509e186b39d7219342ae89764fba04539
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-01 18:32:55 +0300

    Add project files.

---
+++ ChildForm/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChildForm
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            this.LostFocus += Form2_LostFocus;
+        }
+
+        private void Form2_LostFocus(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
